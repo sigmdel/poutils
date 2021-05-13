69ae8b0bc918e049b8ffe34d5337215541f39ab1
sigmdel <none@none>
2021-05-13 18:28:09 -0300

    Added build for Windows

---
+++ build.bat
@@ -0,0 +1,6 @@
+@echo off
+FOR %%S IN (poclean pocopy pofill poignore poinfo pomerge posort postrip poswap poupdate) DO (
+C:\lazarus\lazbuild -B -q --bm=Release %%S.d\%%S.lpi
+move %%S.d\%%S.exe bin\
+rd /S /Q %%S.d\lib
+)
